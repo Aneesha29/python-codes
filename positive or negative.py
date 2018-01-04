a1f4b3467e046fca3d47ea7ee2ee37f378d4fa23
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-04 23:25:42 +0530

    Update positive or negative.py

--- positive or negative.py
+++ positive or negative.py
@@ -1,5 +1,8 @@
+#CHECKE WHEATHER NUMBER IS POSITIVE OR NEGATIVE
 num=int(input("Enter a number between 1&10000: "))
-if (num>1)and(num<100000):
-	print("positive")
+if (num>=1)and(num<=100000):
+	print ("Positive")
+elif (num<0):
+	print ("Negative")
 else:
-	print("negative")
+	print ("0")
