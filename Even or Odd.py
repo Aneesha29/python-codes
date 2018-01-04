e5652c856542e75d0c71e869166cb3c0fbe0f3da
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-05 00:31:31 +0530

    Update Even or Odd.py

--- Even or Odd.py
+++ Even or Odd.py
@@ -1,8 +1,9 @@
+#CHECK WEATHER EVEN OR ODD
 num=int(input("Enter a number between 1&100000: "))
-if (num>=1)and(num<=100000):
+if num in range(1,100000):
 	if (num%2==0):
 		print ("Even")
-elif (num%2!=0):
-	print ("Odd")
+	else:
+		print ("Odd")
 else:
-	print ("enter correct number")
+	print ("Enter a correct number between 1&100000")
