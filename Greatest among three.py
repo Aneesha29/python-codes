d0ff1d88b385b4c81d506badca2561c6fcd1fa2a
soundarsurya <35110076+soundarsurya@users.noreply.github.com>
2018-01-05 20:18:49 +0530

    Create Greatest among three.py

---
+++ Greatest among three.py
@@ -0,0 +1,5 @@
+#FIND GREATEST AMONG THREE NUMBER
+num1=float(input("Enter 1st number: "))
+num2=float(input("Enter 2nd number: "))
+num3=float(input("Enter 3rd number: "))
+print (max(num1,num2,num3))
